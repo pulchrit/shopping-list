76513129db0bdc17234fa273d740516a6ef1d081
Melissa Lafranchise <pulchrit@gmail.com>
2019-03-09 13:17:34 -0800

    check uncheck functionality enabled

--- index.js
+++ index.js
@@ -2,13 +2,26 @@
 
 function operateShoppingList() {
 
-    // Listen for form submit
+    // Changes milk's button from check to un-check...I'm doing this because we aren't 
+    // supposed to change the html in anyway. I added the extra functionality of 
+    // changing the button's text, so I need to dynamically update the check button for 
+    // milk to un-check. If this program was built from scratch and working from an empty 
+    // shopping list, this portion of code would be unnecessary. 
+    let wrongButtonLabel = $(".shopping-item__checked").next().find('.button-label').eq(0);
+    if (wrongButtonLabel.text() === 'check') {
+        wrongButtonLabel.text('un-check');
+    } 
+
+    // Listen for form submit to add items.
     $("#js-shopping-list-form").submit(function(event) {
-        // Prevent form from trying to submit to a server
+        
+        // Prevent form from trying to submit to a server.
         event.preventDefault();
     
-        // Get item entered
+        // Get item entered.
         const itemEntered = $(this).find("#shopping-list-entry").val();
+        
+        // Clear text box for subsequent entries.
         $("#shopping-list-entry").val("");
 
         let newItem = $(`<li>
@@ -23,14 +36,34 @@ function operateShoppingList() {
         </div>
       </li>`);
 
-        // Add new <li> with new shopping list item
-        $(".shopping-list").prepend(newItem);
-        
-        // Clear text box for subsequent entries
-        //itemEntered.val("");
-        
+        // Add new <li> with new shopping list item.
+        $(".shopping-list").prepend(newItem);     
     });
 
+    // Remove list item when delete button is clicked.
+    $("ul").on("click", ".shopping-item-delete", function(event) {
+       
+        // Remove the closest <li> up the DOM to this remove button.
+        this.closest("li").remove();
+    });
+
+    // Mark item as checked making this toggle on/off strikethrough styling
+    // as well as updating the text of the button itself from check to un-check.
+    $("ul").on("click", ".shopping-item-toggle", function(event) {
+        
+        // Toggle strikethrough styling on/off.
+        // Target the span that is the prev() element to this element's parent().
+        let thisShoppingItem = $(this).parent().prev();
+        thisShoppingItem.toggleClass("shopping-item__checked");
+
+        // Toggle text of button label from check to un-check and vice-versa.
+        // Target label of this button by calling children() on the button.
+        let thisButtonLabel = $(this).children();
+        thisButtonLabel.text() === 'check' ? thisButtonLabel.text('un-check') : thisButtonLabel.text('check'); 
+    });
+
+    
+    
 }
 
 $(operateShoppingList());
